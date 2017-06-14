8edf7ac420414ffebaf979e48cdeb37ab3479086
Neil Thaker <neilthaker07@gmail.com>
2017-06-14 16:51:05 -0700

    added some code

--- sorting/src/arraysStrings/CharNums.java
+++ sorting/src/arraysStrings/CharNums.java
@@ -1,7 +1,9 @@
 package arraysStrings;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CharNums {
@@ -11,7 +13,7 @@ public class CharNums {
 		Scanner s = new Scanner(System.in);
 		String s1 = s.next();
 		
-		List<String> op = new ArrayList<String>();
+		List op = new ArrayList();
 		//char[] op=new char[s1.length()*2];
 		int i=0;
 		while(i<s1.length())
@@ -32,12 +34,16 @@ public class CharNums {
 			op[io+1]=(char)c;*/
 			
 			op.add(s1.charAt(i));
-			op.add((char)c);
+			op.add(c);
 			i+=c;
 		}
 		if(op.size()<s1.length())
 		{
-			System.out.println(op);
+			Iterator i1 = op.iterator();
+			while(i1.hasNext())
+			{
+				System.out.print(i1.next());
+			}
 		}
 		else
 		{
