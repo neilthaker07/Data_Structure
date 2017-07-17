9d35d876ba3a9b41d16155dca49b3d9533ad3ec1
Neil Thaker <neilthaker07@gmail.com>
2017-07-17 10:04:47 -0700

    ds

--- .sonarlint/modules/code.30.days/issues/index.pb
+++ .sonarlint/modules/code.30.days/issues/index.pb
@@ -3,3 +3,15 @@ M
 src/progs/AbstractSample.java,d/5/d557d2dc3ed5b914744c323e05298ee4aca0bc4b
 N
 src/progs/StackOperations.java,2/f/2f7a3cd05af3b2d32a4278526209183e31301cdb
+M
+src/progs/DifferenceHack.java,d/7/d7b129daed47e4d3c42c581b267dd3064daa79ee
+H
+src/progs/BSTHeight.java,a/2/a2ec2533e88a6460aa902837f7c3f3463bdaf97d
+F
+src/progs/TopView.java,6/0/60ae5ac94dce91c522b58bf4e9514aa66b947dc9
+I
+src/progs/LevelOrder.java,3/1/31e809323317e0059f79b9f768dbe896034e3884
+S
+#src/progs/LowestCommonAncestor.java,2/2/22907ad5432eccf0077968351a4a123a3c6168e7
+D
+src/progs/IsBST.java,a/c/ac0a740eccba96e2756fcf9180bd25eb57c5e531
--- code.30.days/src/progs/IsBST.java
+++ code.30.days/src/progs/IsBST.java
@@ -1,28 +1,71 @@
 package progs;
 
+import java.util.Scanner;
+
 public class IsBST {
 
-	
-	Node22 root = null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		IsBST a = new IsBST();
+
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node12 root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
 		
-		a.checkBST(a.root);
+		System.out.println(checkBST(root));
+	}
+
+	public static Node12 insert(Node12 root, int data) {
+		if (root == null) {
+			return new Node12(data);
+		} else {
+			Node12 cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
 	}
 	
-	boolean checkBST(Node22 root) 
+	
+	public static boolean checkBST(Node12 root)
 	{
-        
+		if (root == null) 
+		{
+			return false;
+		}
 		
-		return false;
-    }
-
+		return process(root);
+	}
+	
+	public static boolean process(Node12 node)
+	{
+		if( (node.left!=null && node.left.data > node.data) || (node.right!=null && node.right.data <= node.data) )
+		{
+			return false;
+		}
+		
+		if(node.left!=null && !isLeaf(node.left))
+		{
+			process(node.left);
+		}
+		if(node.right!=null && !isLeaf(node.right))
+		{
+			process(node.right);
+		}
+		return true;
+	}
+	
+	static boolean isLeaf(Node12 node)
+	{
+		return node.left!=null || node.right!=null ? false : true;
+	}
 }
-
-
-class Node22 {
-    int data;
-    Node22 left;
-    Node22 right;
- }
